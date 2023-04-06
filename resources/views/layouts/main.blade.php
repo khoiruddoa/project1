593e467239d2b5b85e977654b46a1d9c5bea5aa9
khoiruddoa <khoiruddoa1995@gmail.com>
2023-04-06 16:24:59 +0700

    ubah lagi

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.jsdelivr.net/npm/alpinejs@3"></script>
   <link href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="resources/css/app.css">
+  <link rel="stylesheet" href="./resources/css/app.css">
 </head>
 <body>
     @include('partials.navbar')
