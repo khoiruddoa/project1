aa27df0401329d55216d86e024ba8ad3aa1c305a
khoiruddoa <khoiruddoa1995@gmail.com>
2023-04-10 15:57:11 +0700

    test

--- app/Http/Controllers/ChangePhotoPasswordController.php
+++ app/Http/Controllers/ChangePhotoPasswordController.php
@@ -18,40 +18,43 @@ class ChangePhotoPasswordController extends Controller
         $validator = Validator::make($request->all(), [
             'photo' => 'required|image|max:2048',
         ]);
-    
+
         // Cek apakah ada kesalahan validasi pada $validator
         if ($validator->fails()) {
             $errors = $validator->errors()->all();
             Alert::warning('Gagal', implode('<br>', $errors));
             return back();
         }
-    
-        // Mendapatkan extension dari file gambar
-        $extension = $request['photo']->getClientOriginalExtension();
-    
-        // Memberikan hash name pada file gambar untuk menghindari duplikasi nama file
-        $hashName = hash('sha256', time() . $request['photo']->getClientOriginalName()) . '.' . $extension;
-    
-        // Simpan foto ke dalam storage dengan hash name
-        $request['photo']->storeAs('public/photos', $hashName);
 
-        $path = 'storage/photos/' . $hashName;
-    
+
+        
+
+        // Simpan foto ke dalam storage dengan hash name
+        $file = $request->file('photo'); // mendapatkan file dari request
+        $hashedName = $file->hashName(); // mendapatkan nama file yang di-hash
+        // menyimpan file dengan nama yang di-hash
+
+        $file->move('photos', $hashedName);
+        
+
+        $path = 'photos/' . $hashedName;
+
         $id = Auth()->user()->id;
-    
+
         // Ambil record foto dari database berdasarkan id
         $photo = User::find($id);
-    
+
         // Update path foto di dalam database dengan path yang baru
         $photo->photo = $path;
         $photo->save();
-    
+
         Alert::success('Berhasil', 'Foto berhasil diganti');
         return back();
     }
 
-    
-    public function changePassword(Request $request){
+
+    public function changePassword(Request $request)
+    {
 
         $validator = Validator::make($request->all(), [
             'current_password' => 'required',
@@ -62,7 +65,7 @@ class ChangePhotoPasswordController extends Controller
             Alert::warning('Gagal', implode('<br>', $errors));
             return back();
         }
-        
+
         $user = Auth::user();
 
         if (!Hash::check($request->current_password, $user->password)) {
@@ -73,16 +76,15 @@ class ChangePhotoPasswordController extends Controller
 
 
         $id = Auth()->user()->id;
-    
+
         // Ambil record foto dari database berdasarkan id
         $user = User::find($id);
-    
+
         // Update path foto di dalam database dengan path yang baru
-    
+
         $user->save(['password' => Hash::make($request->password)]);
 
         Alert::success('Berhasil', 'Password berhasil diganti');
         return back();
     }
-    
 }
