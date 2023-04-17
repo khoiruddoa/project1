fb109e593e709ab3a0586e169326d6656d57f0dc
khoiruddoa <khoiruddoa1995@gmail.com>
2023-04-17 11:57:47 +0700

    array map

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -88,9 +88,30 @@ class ChartController extends Controller
         $totals[] = $detail->total_transactions; 
     }
 
+    $bulan = array(
+        1 => 'Januari',
+        2 => 'Februari',
+        3 => 'Maret',
+        4 => 'April',
+        5 => 'Mei',
+        6 => 'Juni',
+        7 => 'Juli',
+        8 => 'Agustus',
+        9 => 'September',
+        10 => 'Oktober',
+        11 => 'November',
+        12 => 'Desember'
+    );
+
+
+    $namaBulan = array_map(function($month) use ($bulan) {
+        return $bulan[$month];
+    }, $months);
+    
+
         return view('dashboard.graphic.jumlahtransaksi', [
             
-            'months' => $months, 'totals' => $totals
+            'months' => $namaBulan, 'totals' => $totals
         ]);
     }
 
