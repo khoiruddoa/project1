a1444cd62785d1f65e1224b056ce1c62ddded3d7
khoiruddoa <khoiruddoa1995@gmail.com>
2023-04-27 12:44:19 +0700

    aneh

--- resources/views/dashboard/transaksipengepul/index.blade.php
+++ resources/views/dashboard/transaksipengepul/index.blade.php
@@ -258,14 +258,14 @@
 
 
                                             <!-- Modal toggle -->
-                                            <button data-modal-target="authentication-modal{{ $transaction->id }}"
-                                                data-modal-toggle="authentication-modal{{ $transaction->id }}"
+                                            <button data-modal-target="authentication-modal{{ $transaction->id }}payment"
+                                                data-modal-toggle="authentication-modal{{ $transaction->id }}payment"
                                                 class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                                 type="button">
                                                 Pembayaran
                                             </button>
                                             <!-- Main modal -->
-                                            <div id="authentication-modal{{ $transaction->id }}" tabindex="-1"
+                                            <div id="authentication-modal{{ $transaction->id }}payment" tabindex="-1"
                                                 aria-hidden="true"
                                                 class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
                                                 <div class="relative w-full h-full max-w-md md:h-auto">
@@ -273,7 +273,7 @@
                                                     <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                                                         <button type="button"
                                                             class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-sidebar dark:hover:text-white"
-                                                            data-modal-hide="authentication-modal{{ $transaction->id }}">
+                                                            data-modal-hide="authentication-modal{{ $transaction->id }}payment">
                                                             <svg aria-hidden="true" class="w-5 h-5" fill="currentColor"
                                                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                                 <path fill-rule="evenodd"
