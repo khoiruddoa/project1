37d63898fe99040ba429e05444098519c4cf4bd3
khoiruddoa <khoiruddoa1995@gmail.com>
2023-05-11 09:44:10 +0700

    revisi

--- resources/views/dashboard/transaksipengepul/index.blade.php
+++ resources/views/dashboard/transaksipengepul/index.blade.php
@@ -16,9 +16,12 @@
                 </button>
 
                 <!-- Main modal -->
-                <div id="authentication-modalPengepul" tabindex="-1" aria-hidden="true"
+                {{-- <div id="authentication-modalPengepul" tabindex="-1" aria-hidden="true"
                     class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
-                    <div class="relative w-full h-full max-w-md md:h-auto">
+                    <div class="relative w-full h-full max-w-md md:h-auto"> --}}
+
+                        <div id="authentication-modalPengepul" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+                            <div class="relative w-full max-w-2xl max-h-full">
                         <!-- Modal content -->
                         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
                             <button type="button"
@@ -90,7 +93,7 @@
 
                                                                             <button type="submit"
                                                                                 onclick="this.disabled=true; this.form.submit();"
-                                                                                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buat
+                                                                                class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Buat
                                                                                 Transaksi Pengepul</button>
                                                                         </div>
                                                                     </div>
