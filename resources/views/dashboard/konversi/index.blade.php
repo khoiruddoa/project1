2eddbe51dae414ce3b6dff557b482673d0be8511
khoiruddoa <khoiruddoa1995@gmail.com>
2023-05-25 16:07:13 +0700

    ubah lagi

--- resources/views/dashboard/konversi/index.blade.php
+++ resources/views/dashboard/konversi/index.blade.php
@@ -220,7 +220,7 @@
                                 <div>
                                     <label for="name"
                                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Berat(gram)</label>
-                                    <input type="number" name="gram" id="qty" step="0.01"
+                                    <input type="number" name="gram" id="qty" step="0.001"
                                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                         required>
                                 </div>
